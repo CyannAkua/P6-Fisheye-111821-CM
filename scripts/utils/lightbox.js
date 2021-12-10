f93c7a16ed097a37c1bcf10de5243e3796e422c7
AkuaSin <aquanet200@gmail.com>
2021-12-10 17:20:42 +0100

    carrousel

---
+++ scripts/utils/lightbox.js
@@ -0,0 +1,64 @@
+const lightboxBg = document.getElementsByClassName("lightbox_bg")[0];
+const articlePic = document.getElementsByClassName("media-section")[0];
+var currentTarget = [];
+const previousArrow = document.getElementsByClassName("lightbox-previous")[0];
+const nextArrow = document.getElementsByClassName("lightbox-next")[0];
+const imageName = document.getElementsByClassName("image-name")[0];
+const imageHold = document.getElementsByClassName("image-hold")[0];
+const videoHold = document.getElementsByClassName("video-hold")[0];
+const closeCross = document.getElementsByClassName("lightbox-close")[0];
+
+function displayLightbox() {
+  lightboxBg.style.display = "block";
+}
+function closeLightbox() {
+  lightboxBg.style.display = "none";
+}
+closeCross.addEventListener("click", closeLightbox);
+articlePic.addEventListener("click", (e) => {
+  if (e.target !== e.currentTarget) {
+    currentTarget = e.target.parentElement;
+    displayLightbox();
+    pic();
+  }
+});
+//lightboxBg.children[0].addEventListener("click", (e) => {
+//if (e.target !== e.currentTarget) {
+//} else {
+//closeLightbox();
+//}
+//});
+previousArrow.addEventListener("click", (e) => {
+  currentTarget = currentTarget.previousSibling;
+  pic();
+});
+nextArrow.addEventListener("click", (e) => {
+  currentTarget = currentTarget.nextSibling;
+  pic();
+});
+function pic() {
+    if(currentTarget.firstChild.tagName == 'VIDEO'){
+        videoHold.children[0].src = currentTarget.children[0].src;
+        videoHold.style.display = "block";
+        imageHold.style.display = "none";
+        
+    }
+  else {imageHold.style.display="";
+  videoHold.style.display = "none";
+  imageHold.src = currentTarget.children[0].src;}
+  imageName.innerText = currentTarget.children[1].innerText;
+  checkArrow();
+}
+
+function checkArrow() {
+  if (currentTarget.nextSibling === null) {
+    nextArrow.style.display = "none";
+  } else {
+    nextArrow.style.display = "";
+  }
+  if (currentTarget.previousSibling === null) {
+    previousArrow.style.display = "none";
+  } else {
+    previousArrow.style.display = "";
+  }
+}
